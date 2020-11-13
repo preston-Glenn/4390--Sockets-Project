0b7c8b1a0c5fa577b4c2db07a34bb407467a3dbf
WalmartDeli <55575167+WalmartDeli@users.noreply.github.com>
2020-11-13 17:09:14 -0600

    Update TCPServer.java
    Having issue with dataoutputstream

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -1,34 +1,101 @@
 import java.io.*; 
 import java.net.*; 
 
-class TCPServer { 
-
-  public static void main(String argv[]) throws Exception 
-    { 
-      String clientSentence; 
-      String capitalizedSentence; 
-
-      ServerSocket welcomeSocket = new ServerSocket(6789); 
-
-      while(true) { 
-  
-            Socket connectionSocket = welcomeSocket.accept(); 
-
-           BufferedReader inFromClient = 
-              new BufferedReader(new
-              InputStreamReader(connectionSocket.getInputStream())); 
 
 
-           DataOutputStream  outToClient = 
-             new DataOutputStream(connectionSocket.getOutputStream()); 
-
-           clientSentence = inFromClient.readLine(); 
-
-           capitalizedSentence = clientSentence.toUpperCase() + '\n'; 
-
-           outToClient.writeBytes(capitalizedSentence); 
+class ClientHandler implements Runnable{  
+	private Socket clientSocket;
+	final BufferedReader inFromClient;
+	final DataOutputStream outToClient;
+	private String name;
+	  
+	ClientHandler(Socket clientSocket, BufferedReader in, DataOutputStream out) {
+		this.clientSocket = clientSocket;
+		this.inFromClient = in;
+		this.outToClient = out;
+	  } 
+	  
+	public void run() {
+		try {
+			// Input/Output streams
+			while(true) {
+				String clientSentence = this.inFromClient.readLine(); 
+				System.out.println("Recieved: " + clientSentence);
+					
+				String messageType = clientSentence.substring(0,6);
+				String message = clientSentence.substring(6);
+				System.out.println(messageType);
+				System.out.println(message);
+				if(messageType.equals("NAME--")) {
+					this.name = message;
+					System.out.println(name);
+				}
+				else if (messageType.equals("MATH--")) {
+					// math calculation here
+					System.out.println("ENter math");
+					this.outToClient.writeBytes(message);
+				}
+				else {
+					// close connection
+					System.out.println("Connection Closed");
+					this.clientSocket.close();
+					break;
+				}
+				
+			}
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+        try
+        { 
+            // closing resources 
+            this.inFromClient.close(); 
+            this.outToClient.close(); 
+              
+        }catch(IOException e){ 
+            e.printStackTrace(); 
         } 
-    } 
+		
+	}
+}
+
+class TCPServer { 
+	 private static final int PORT_NUMBER = 6789;
+	    private static ServerSocket serverSocket;
+	    private static ClientHandler clientHandler;
+	    private static Thread thread;
+
+	    public static void main(String[] args) throws IOException {
+	        serverSocket = new ServerSocket(PORT_NUMBER);
+
+	        while (true) {
+	        	Socket s = null;
+	        	try {
+	        		
+	        		s = serverSocket.accept();
+	        		
+	        		BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+					DataOutputStream  out = new DataOutputStream(s.getOutputStream()); 
+					
+		            clientHandler = new ClientHandler(s, in, out);
+		            thread = new Thread(clientHandler);
+		            thread.start();
+		            
+	        	}
+	        	catch (Exception e) {
+	        		s.close();
+	        		e.printStackTrace();
+	        	}
+
+	        }
+	    }
+
+	    protected void finalize() throws IOException {
+	        serverSocket.close();
+	    }
 } 
  
 // Client 1 send connection request-- Server creats connection and logs name of user
